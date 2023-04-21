958f5a905a45c605dd5423a0c637943c00edec35
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-04-21 14:50:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 27619245
 API_HASH     = "b06905ffc083d4f7d78b5fbd5923bb84"
 BOT_TOKEN    = "6220002313:AAGTYq0tWyyXQpdHi_plKs31WMyzFK9jC5I"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
+DATABASE_URI = "mongodb+srv://kailash:pass@cluster0.sqtztxm.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001896609847
+ADMIN        = 6133440326
 CHANNEL      = "@GreyMatter_Bots"
