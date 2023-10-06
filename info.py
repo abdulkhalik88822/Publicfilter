3340c672cbd46be18924ea2f866c73ad0b360a45
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-10-06 19:05:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 API_ID       = 27619245
 API_HASH     = "b06905ffc083d4f7d78b5fbd5923bb84"
-BOT_TOKEN    = "6254854432:AAF7ZQM67_8DiKQS0Or_M7NHk3o58x5yku0"
+BOT_TOKEN    = "6264487502:AAHZMluG6JYyJ_2SVuYDYispJjoM8jeER3o"
 SESSION      = "BQFG94kAcQrCzRuHH3Dcljnpux35Ow44ZaKpijPDqHqFTAswAQdZcJwttUPm1UuowX3tL4BLQ9pGG6FtwBTdG8xRLFC5wpBjZ57JJzD0of7LqXY5fmEa4I863hKLn6uQIB_1SJgEpBS8PqJOeN56cnIIaKkoZTehkh6zLUYgWJbfAto74ub_n_oH3OIb4Tj589dg_9FfrZkxRztatHthoHrkhOpvSC5YgyTsIjPd-hXpE1TuEgAPY9PbWok9upIoMySmb0JhpKQCjKoBIkdMF67X6yQQ2wRtay3jDOTBInm_gwTZCmYmfD-8pXZ-xR0XeHa0SvaMY_T6q9GKV17AVjM0GdX8yQAAAAFnW0CKAA"
-DATABASE_URI = "mongodb+srv://abdul789:abdul789@cluster0.gdjllja.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://abdul:abdul@cluster0.tz24m3k.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001626107740
 ADMIN        = 6133440326
 CHANNEL      = "-1001836465539"
